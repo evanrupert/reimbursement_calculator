70baf5d235510e00721cde8b54802b4aa778c308
Evan Rupert <evan@studioreach.io>
2022-06-28 17:37:56 -0400

    Create basic reimbursement calculation algorithm

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-console.log('Hello, World!');
+const { DateTime } = require('luxon');
+
+const FULL_DAY_COST = {
+  Low: 75,
+  High: 85,
+};
+
+const TRAVEL_DAY_COST = {
+  Low: 45,
+  High: 55,
+};
+
+function isFullDay(activeDates, date) {
+  const previousDayInSeconds = date.minus({ days: 1 }).toSeconds();
+  const nextDayInSeconds = date.plus({ days: 1 }).toSeconds();
+
+  return activeDates.has(previousDayInSeconds) && activeDates.has(nextDayInSeconds);
+}
+
+function convertProjectListToDateMap(set) {
+  const activeDates = new Map();
+
+  set.forEach((project) => {
+    let date = project.startDate;
+    while (date <= project.endDate) {
+      // TODO: If same date was previously entered with a low cost,
+      //        change to high cost since it takes precedent
+      activeDates.set(date.toSeconds(), project.cityCost);
+
+      date = date.plus({ days: 1 });
+    }
+  });
+
+  return activeDates;
+}
+
+exports.calculateReimbursement = (set) => {
+  if (set.length === 0) {
+    return 0;
+  }
+
+  const activeDates = convertProjectListToDateMap(set);
+
+  let totalCost = 0;
+
+  activeDates.forEach((cost, dateInSeconds) => {
+    const date = DateTime.fromSeconds(dateInSeconds);
+
+    const fullDay = isFullDay(activeDates, date);
+
+    console.log(`${date.toString()}: ${fullDay}`);
+
+    totalCost += fullDay ? FULL_DAY_COST[cost] : TRAVEL_DAY_COST[cost];
+  });
+
+  return totalCost;
+};
--- test.js
+++ test.js
@@ -1,5 +1,36 @@
-describe('The Program', () => {
-  it('should work', () => {
-    expect(true).toBeTrue();
+const { DateTime } = require('luxon');
+const lib = require('./index');
+
+describe('Reimbursement Calculator', () => {
+  it('should calculate reimbursement for a single project', () => {
+    const result = lib.calculateReimbursement([{
+      startDate: DateTime.local(2015, 9, 1),
+      endDate: DateTime.local(2015, 9, 3),
+      cityCost: 'Low',
+    }]);
+
+    expect(result).toBe(165);
+  });
+
+  it('should calculate reimbursement for multiple projects', () => {
+    const result = lib.calculateReimbursement([
+      {
+        startDate: DateTime.local(2015, 9, 1),
+        endDate: DateTime.local(2015, 9, 3),
+        cityCost: 'Low',
+      },
+      {
+        startDate: DateTime.local(2015, 9, 5),
+        endDate: DateTime.local(2015, 9, 7),
+        cityCost: 'High',
+      },
+      {
+        startDate: DateTime.local(2015, 9, 8),
+        endDate: DateTime.local(2015, 9, 8),
+        cityCost: 'High',
+      },
+    ]);
+
+    expect(result).toBe(445);
   });
 });
